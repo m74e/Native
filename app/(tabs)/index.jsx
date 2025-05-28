a81a46bb3499a32ced9c0aa7f6f124398dec63d9
Muhammed Al-lami <105250911+m74e@users.noreply.github.com>
2025-05-28 11:41:45 +0300

    Remove unused tab layout and explore components; add contact page with navigation link

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,12 +1,28 @@
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
 import React from "react";
 import icedCoffeeImage from "@/assets/images/iced-coffee.png";
+import { Link } from "expo-router";
 
 const app = () => {
   return (
     <View style={styles.container}>
       <ImageBackground source={icedCoffeeImage}>
         <Text style={styles.text}>coffee shop</Text>
+        <Link
+          // style={[styles.link, { marginHorizontal: "auto" }]}
+          href={"/contact"}        
+        >
+          {" "}
+          <Pressable style={styles.Button}>
+            <Text style={styles.Buttontext}>Contact</Text>{" "}
+          </Pressable>
+        </Link>
       </ImageBackground>
     </View>
   );
@@ -25,13 +41,38 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
-    
   },
   text: {
     fontSize: 24,
     color: "white",
     fontWeight: "bold",
-    textAlign: "center", 
+    textAlign: "center",
     backgroundColor: "rgba(0, 0, 0, 0.5)",
+    marginBottom: 120,
+  },
+  link: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 4,
+  },
+  Button: {
+    height: 60,
+    borderRadius: 20,
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+    padding: 6,
+  },
+  Buttontext: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 4,
   },
 });
